6fccc93cf63c34c53273a368ac08549ff81049bb
Carla Galarza <cmg2228@columbia.edu>
2018-11-26 15:40:38 -0500

    Only creating one URIService.solr object. Adding helper for solr suffix

--- lib/uri_service.rb
+++ lib/uri_service.rb
@@ -1,7 +1,19 @@
 module URIService
   thread_mattr_accessor :solr_connection
 
+  SOLR_SUFFIX = {
+    'string'  => '_si',
+    'number'  => '_ii',
+    'boolean' => '_bi'
+  }.freeze
+
+  # TODO: Write test to make sure only one object is created.
   def self.solr
-    @solr_connection ||= URIService::Solr.new
+    self.solr_connection = URIService::Solr.new unless solr_connection
+    solr_connection
+  end
+
+  def self.solr_suffix(data_type)
+    SOLR_SUFFIX[data_type]
   end
 end
