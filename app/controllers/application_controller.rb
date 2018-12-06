774c3892cfbd2f78b44aff784908983eb3824e16
Carla Galarza <cmg2228@columbia.edu>
2018-12-06 10:27:35 -0500

    Requiring token authentication for all requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
-  before_action :ensure_json_request
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_token, :ensure_json_request
 
   private
     def vocabulary
@@ -15,5 +17,22 @@ class ApplicationController < ActionController::API
     def ensure_json_request
       return if request.format == :json
       head :not_acceptable
+    end
+
+    def authenticate_token
+      unless authentication_status == :ok
+        render json: { errors: [{ title: authentication_status.to_s.titlecase }] }.to_json,
+               status: authentication_status
+      end
+    end
+
+    def authentication_status
+      status = :unauthorized
+      authenticate_with_http_token do |token, options|
+        Rails.application.config_for(:uri_service).fetch('api_keys', []).tap do |valid_api_key|
+          status = (valid_api_key.include?(token)) ? :ok : :forbidden
+        end
+      end
+      status
     end
 end
