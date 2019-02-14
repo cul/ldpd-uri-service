c0015b39ccc44da305d94b3b980353e84c597fc0
Carla Galarza <cmg2228@columbia.edu>
2019-02-14 17:15:00 -0500

    Checking that authentication is required for /admin/commit

--- spec/requests/v1/admin_spec.rb
+++ spec/requests/v1/admin_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Admin' do
   describe '#commit' do
+    include_examples 'authentication required', 'post', '/api/v1/admin/commit'
+
     it 'returns successful response' do
       post_with_auth '/api/v1/admin/commit'
       expect(response.status).to be 204
